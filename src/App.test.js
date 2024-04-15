3fabd60f4d0f1d195f22cb12d1b66ccde61b4b57
Luis Cedeno <59905362+Lcedeno14@users.noreply.github.com>
2024-04-15 09:31:21 -0700

    testing the webpack

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
 import ChatbotComponent from './chatbot1';
 import config from './chatbotConfig';
 import MessageParser from './MessageParser';
